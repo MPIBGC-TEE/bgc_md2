2fb939a3e1adbe2846c90d2aa54e97461de30321
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-07-01 10:55:25 +0200

    corrected the wrong function calls

--- bgc_md2/models/cable_all/cable_transit_time/example_runs/parallel_1901_1903_with_minimum_spinup.sh
+++ bgc_md2/models/cable_all/cable_transit_time/example_runs/parallel_1901_1903_with_minimum_spinup.sh
@@ -6,5 +6,5 @@ bN=$(basename $0) #this scripts name
 runDirName="${bN%.sh}"
 run_and_log_in_target_dir \
   ${cableDataDir} \
-  "cable_run_with_spinup $runDirName 1901 1902 1 1 1901 1903" \
+  "cable_run_with_spinup $runDirName 1901 1902 1 1 1903" \
   "log_stdout_and_stderr_${runDirName}"
--- bgc_md2/models/cable_all/cable_transit_time/example_runs/parallel_1901_2004_with_spinup.sh
+++ bgc_md2/models/cable_all/cable_transit_time/example_runs/parallel_1901_2004_with_spinup.sh
@@ -6,5 +6,5 @@ bN=$(basename $0) #this scripts name
 runDirName="${bN%.sh}"
 run_and_log_in_target_dir \
   ${cableDataDir} \
-  "cable_run_with_spinup $runDirName 1901 1910 5 10 1901 2004" \
+  "cable_run_with_spinup $runDirName 1901 1910 5 10 2004" \
   "log_stdout_and_stderr_${runDirName}"
