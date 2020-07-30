found and corrected an error in the graph computation that had led to missed paths. 2 Test still failing

bgc_md2/models/Williams2005GCB/source.py
@@ -6,6 +6,8 @@ from sympy.physics.units import (
         gram,meter,day,kelvin)
 from sympy.physics.units.systems.si import dimsys_SI
 
+from frozendict import frozendict
+
 from bgc_md2.resolve.mvars import (
         CompartmentalMatrix,
         InputTuple,
@@ -14,12 +16,12 @@ from bgc_md2.resolve.mvars import (
         VegetationCarbonInputScalar,
         VegetationCarbonInputPartitioningTuple,
         VegetationCarbonStateVariableTuple,
-        QuantityParameterizedModel,
         NumericParameterization,
-        NumericStartValues,
+        NumericStartValueDict,
         NumericSimulationTimes,
         QuantityParameterization,
-        QuantityModelRun
+        QuantityModelRun,
+        QuantityParameterizedModel,
 )
 from bgc_md2.resolve.computers import (
         smooth_reservoir_model_from_input_tuple_and_matrix
@@ -116,7 +118,7 @@ I = InputTuple(u*ImmutableMatrix(b))
 #    possible_combinations:
 #        - ["param_vals", "init_vals", RT1]
 #
-p1 = NumericParameterization(
+np1 = NumericParameterization(
     par_dict={
         NPP		    :Rational(409, 365), #*gram*/(meter**2*day)
         # Note:
@@ -148,12 +150,12 @@ p1 = NumericParameterization(
 	    p_15		:0.001,          #1/day
 	    p_16        :0.251,
     },
-    func_dict={}
+    func_dict=frozendict({})
     #state_var_units=kilogram/meter**2,
     #time_unit=day
 )
-sv1 =   NumericStartValues({C_f: 58, C_lab: 60, C_w: 770, C_r: 102})
-times = NumericSimulationTimes(np.arange(0,1096,1))
+nsv1 =   NumericStartValueDict({C_f: 58, C_lab: 60, C_w: 770, C_r: 102})
+ntimes = NumericSimulationTimes(np.arange(0,1096,1))
 
 # Open questions regarding the translation
 # - The variable G seems to be identical with GPP but
@@ -162,6 +164,9 @@ specialVars={
     I, # the overall imput
     t, # time for the complete system
     x, # state vector of the the complete system
+    np1,
+    nsv1,
+    ntimes,    
     #
     # the following variables give a more detailed view with respect to
     # carbon and vegetation variables

bgc_md2/resolve/mvars.py
@@ -17,10 +17,7 @@ from sympy import (
 from sympy.physics.units import Quantity
 from sympy.physics.units.systems import SI
 from sympy.physics.units import (
-    day,
-    year,
-    kilogram,
-    gram
+   time 
 )
 from CompartmentalSystems.smooth_reservoir_model import SmoothReservoirModel
 from CompartmentalSystems.smooth_model_run import SmoothModelRun
@@ -30,27 +27,112 @@ from bgc_md2.described_quantities import (
     to_number
 )
 
-class NumericStartValues(frozendict):
+# fixme: mm 03-12-2020
+# At the moment the classes are just defined to provide
+# the vocabulary for the computer signatures and model 
+# descriptions.
+# Ultimately the classes should have sensible constructors
+# that check their arguments thoroughly to expose
+# inadequate imput early in the process 
+# part of this should be a dimensional analysis
+# which requires that the model authors provide dimensions
+# for there variables
+
+class InFluxesBySymbol(frozendict):
     pass
+
+
+class OutFluxesBySymbol(frozendict):
+    pass
+
+
+class InternalFluxesBySymbol(frozendict):
+    pass
+
+
+class TimeSymbol(Symbol):
+    # should become a quantity with dimension time
+    pass
+
+
+class StateVariableTuple(tuple):
+    pass
+
+
+class InputTuple(tuple):
+    pass
+
+
+class CompartmentalMatrix(ImmutableMatrix):
+    pass
+
+
+# vegetation specific variables
+class VegetationCarbonInputTuple(tuple):
+    pass
+
+
+class VegetationCarbonInputScalar(Expr):
+    pass
+
+
+class VegetationCarbonInputPartitioningTuple(tuple):
+    pass
+
+
+class VegetationCarbonStateVariableTuple(tuple):
+    pass
+
+
+class VegetationCarbonCompartmentalMatrix(ImmutableMatrix): #cycling matrix
+    pass
+
+
+class NumericStartValueDict(frozendict):
+    pass
+
+
+class QuantityStartValueDict(frozendict):
+    pass
+
 
 
 # extending ndarray is special
 # https://numpy.org/doc/stable/user/basics.subclassing.html
 class NumericSimulationTimes(np.ndarray):
-    def __new__(cls, input_array, info=None):
+    def __new__(cls, input_array ):
         # Input array is an already formed ndarray instance
-        # We first cast to be our class type
+        # We cast to be our class type
         obj = np.asarray(input_array).view(cls)
-        # add the new attribute to the created instance
-        # obj.info = info
-        # Finally, we must return the newly created object:
+        obj.flags.writeable = False
         return obj
 
-    def __array_finalize__(self, obj):
-        # see InfoArray.__array_finalize__ for comments
-        if obj is None: return
-        #self.info = getattr(obj, 'info', None)
-    
+    def __hash__(self):
+        return hash(tuple(self))
+
+class NumericStartValueArray(np.ndarray):
+    def __new__(cls, input_array ):
+        # Input array is an already formed ndarray instance
+        # We cast to be our class type
+        obj = np.asarray(input_array).view(cls)
+        obj.flags.writeable = False
+        return obj
+
+    def __hash__(self):
+        return hash(tuple(self))
+
+
+
+class QuantitySimulationTimes(np.ndarray):
+    def __new__(cls, input_array):
+        # Input array is an already formed ndarray instance
+        # with units attached
+        assert(SI.get_dimensional_expr(input_array[0])==time)
+        obj = np.asarray(input_array).view(cls)
+        return obj
+
+    def __hash__(self):
+        return hash(tuple(self))
 
 
 class NumericParameterization():
@@ -73,8 +155,19 @@ class NumericParameterization():
         par_dict,
         func_dict
     ):
-        self.par_dict = par_dict
-        self.func_dict = func_dict
+        self.par_dict = frozendict(par_dict)
+        self.func_dict = frozendict(func_dict)
+
+
+class NumericParameterizedSmoothReservoirModel():
+    def __init__(
+        self,
+        srm,
+        parameterization
+    ):
+        self.srm                = srm
+        self.parameterization   = parameterization
+
 
 
 class QuantityParameterization(NumericParameterization):
@@ -154,55 +247,4 @@ class QuantityModelRun():
 
         return sol_quant
         
-
-# fixme: mm 03-12-2020
-# At the moment the classes are just defined to provide
-# the vocabulary for the computer signatures and model 
-# descriptions.
-# Ultimately the classes should have sensible constructors
-# that check their arguments thoroughly to expose
-# inadequate imput early in the process 
-# part of this should be a dimensional analysis
-# which requires that the model authors provide dimensions
-# for there variables
-
-class InFluxesBySymbol(frozendict):
-    pass
-
-class OutFluxesBySymbol(frozendict):
-    pass
-
-class InternalFluxesBySymbol(frozendict):
-    pass
-
-class TimeSymbol(Symbol):
-    # should become a quantity with dimension time
-    pass
-
-class StateVariableTuple(tuple):
-    pass
-
-class InputTuple(tuple):
-    pass
-
-class CompartmentalMatrix(ImmutableMatrix):
-    pass
-
-# vegetation specific variables
-class VegetationCarbonInputTuple(tuple):
-    pass
-
-class VegetationCarbonInputScalar(Expr):
-    pass
-
-class VegetationCarbonInputPartitioningTuple(tuple):
-    pass
-
-class VegetationCarbonStateVariableTuple(tuple):
-    pass
-
-class VegetationCarbonCompartmentalMatrix(ImmutableMatrix): #cycling matrix
-    pass
-class ParameterDict(frozendict):
-    pass
 

tests/Test_model_run_with_units.py
@@ -21,10 +21,17 @@ from sympy.physics.units import (
     gram
 )
 from bgc_md2.resolve.mvars import (
-    QuantityParameterizedModel,
     NumericParameterization,
-    QuantityParameterization,
+    NumericStartValueDict,
+    NumericStartValueArray,
+    NumericSimulationTimes,
+    NumericParameterizedSmoothReservoirModel,
+    QuantityParameterizedModel,
     QuantityModelRun
+)
+
+from bgc_md2.resolve.computers import (
+    numeric_model_run_1
 )
 
 from CompartmentalSystems.smooth_reservoir_model import SmoothReservoirModel
@@ -34,7 +41,7 @@ from CompartmentalSystems.smooth_model_run import SmoothModelRun
 
 class TestModelRunWithUnits(unittest.TestCase):
 
-    def test_solve(self):
+    def setUp(self):
         C_0,  C_1, t, k_01, k_10, k_0o = (
                 Symbol(s) for s in ("C_0", "C_1", "t", "k_01", "k_10", "k_0o")
         )
@@ -55,7 +62,7 @@ class TestModelRunWithUnits(unittest.TestCase):
             (1, 0): k_10*C_0*C_1  # flux from pool 1 to pool 0
             }
         time_symbol = t
-        srm = SmoothReservoirModel(
+        self.srm = SmoothReservoirModel(
             state_variables,
             time_symbol,
             inputs,
@@ -63,10 +70,18 @@ class TestModelRunWithUnits(unittest.TestCase):
             internal_fluxes
         )
 
+
+    def test_solve_num(self):
+        C_0,  C_1, t, k_01, k_10, k_0o = (
+                Symbol(s) for s in ("C_0", "C_1", "t", "k_01", "k_10", "k_0o")
+        )
+
+        k_1o = Function('k_1o')
+
         par_dict= {
             k_01: 1/100,  # 1/year
             k_10: 1/100,  # 1/year
-            k_0o: 1/2     # 1/year  
+            k_0o: 1/2     # 1/year
         }
 
         def k_1o_func(t):
@@ -77,14 +92,66 @@ class TestModelRunWithUnits(unittest.TestCase):
             u_res = V_0+V_range*sin(omega*t+phi)
             return u_res
 
-        para = QuantityParameterization(
+        para = NumericParameterization(
+            par_dict=par_dict,
+            func_dict={k_1o: k_1o_func},
+        )
+        npsrm = NumericParameterizedSmoothReservoirModel(
+            srm=self.srm,
+            parameterization=para
+        )
+        # The parameterdict and the functions, possibly even the matrix/flux expressions
+        # have implicit unit assumption, which the user is required to maintain consistently.
+        # To make modelruns comparable it is important to remember the units for which this 
+        # consistency can be guaranteed.
+
+        # A model run can then adapt the units of times and masses since it 
+        times_num= np.linspace(0, 20, 16)
+        start_values_num= NumericStartValueArray([1, 2]) 
+
+
+        #fake model run
+        #times_num=np.array([to_number(tv,qpm.time_unit) for tv in times_quant])
+        #start_values_num=np.array([to_number(sv,qpm.state_var_unit) for sv in start_values_quant])
+        #mr=SmoothModelRun(
+        #    qpm.srm,
+        #    qpm.par_dict,
+        #    start_values_num,
+        #    times_num,
+        #    {k_1o: k_1o_func}
+        #)
+        nmr = numeric_model_run_1(
+            npsrm,
+            start_values_num,
+            times_num
+        )
+        print(nmr.solve())
+
+    @unittest.skip
+    def test_solve(self):
+
+        par_dict= {
+            k_01: 1/100,  # 1/year
+            k_10: 1/100,  # 1/year
+            k_0o: 1/2     # 1/year
+        }
+
+        def k_1o_func(t):
+            omega = 2*pi    # 1/year
+            phi = pi/8
+            V_0 = 20        # kilogram/year
+            V_range = 5     # kilogram/year
+            u_res = V_0+V_range*sin(omega*t+phi)
+            return u_res
+
+        para = NumericParameterizedSmoothReservoirModel(
             par_dict=par_dict,
             func_dict={k_1o: k_1o_func},
             state_var_unit=kilogram, #fixme could become a vector
             time_unit=year
         )
         qpm = QuantityParameterizedModel(
-            srm=srm,
+            srm=self.srm,
             parameterization=para
         )
         # The parameterdict and the functions, possibly even the matrix/flux expressions
@@ -118,4 +185,6 @@ if __name__ == '__main__':
     unittest.main()
 
 
+
+    unittest.main()
 