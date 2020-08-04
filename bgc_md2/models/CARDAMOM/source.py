9cf0687e746543753ff7802de39f3120c62db728
goujou <hmetzler@bgc-jena.mpg.de>
2020-08-04 08:24:42 +0200

    changed path for data in CARDAMOM/source.py

--- bgc_md2/models/CARDAMOM/source.py
+++ bgc_md2/models/CARDAMOM/source.py
@@ -1,13 +1,16 @@
 import xarray as xr
 from CARDAMOMlib import load_mdo
 
-#dataset = xr.open_dataset('~/Desktop/CARDAMOM/cardamom_for_holger.nc')
-dataset = xr.open_dataset('/home/data/CARDAMOM/cardamom_for_holger.nc')
-
 def create_pwc_model_run_fd(ens, lat, lon):
+    #dataset = xr.open_dataset('~/Desktop/CARDAMOM/cardamom_for_holger.nc')
+    dataset = xr.open_dataset('/home/data/CARDAMOM/cardamom_for_holger.nc')
+
     ds = dataset.isel(ens=ens, lat=lat, lon=lon)
     mdo = load_mdo(ds)
     pwc_mr_fd = mdo.create_model_run()
+
+    ds.close()
+    dataset.close()
     return pwc_mr_fd
 
 # examples
