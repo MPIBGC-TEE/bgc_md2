2281f9da7868a9b648c50ba3c3294f33d1dad6df
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-09-11 17:25:21 +0200

    disabled parallel tests to get rid of the unmaintained concurrency test dependency

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,7 +3,7 @@
 # this is a pure python version
 # run with pyhton3 run_tests.py in a venv
 
-from concurrencytest import ConcurrentTestSuite, fork_for_tests
+#from concurrencytest import ConcurrentTestSuite, fork_for_tests
 import unittest
 import sys
 
@@ -15,10 +15,11 @@ def main():
     # p = unittest.defaultTestLoader.discover('', pattern='Pinned_Test*')
     # s.addTests(p)
     # concurrent_suite = s
-    concurrent_suite = ConcurrentTestSuite(s, fork_for_tests(64))
+    #concurrent_suite = ConcurrentTestSuite(s, fork_for_tests(64))
     r = unittest.TextTestRunner()
 
-    res = r.run(concurrent_suite)
+    #res = r.run(concurrent_suite)
+    res = r.run(s)
     if len(res.errors) + len(res.failures) > 0:
         sys.exit(1)
 
