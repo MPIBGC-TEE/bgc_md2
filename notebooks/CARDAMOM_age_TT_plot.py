b84b2476375ae78e1588a609f917d0784fc051d6
goujou <hmetzler@matagorda.bgc.mpg.de>
2020-09-18 11:22:42 +0200

    plot notebook

--- notebooks/CARDAMOM_age_TT_plot.py
+++ notebooks/CARDAMOM_age_TT_plot.py
@@ -13,14 +13,36 @@
 #     name: python3
 # ---
 
+# +
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 import pandas as pd
 
+xr.set_options(display_style='html')
+# -
 
-age_output_dir = 'age_output/'
-filestem = "cardamom_for_holger_10_ensembles"
+age_output_dir = '/home/data/CARDAMOM/output/age_output/'
+#filestem = "cardamom_for_holger_10_ensembles"
 
-ds_age_global_clean = xr.open_mfdataset('age_output/*.nc')
+ds_age_global_clean = xr.open_mfdataset(age_output_dir + '*.nc')
 ds_age_global_clean
+
+# +
+fig, ax = plt.subplots()
+ds_age_global_clean.mean_age_vector.mean(dim=['ens', 'lat', 'lon']).plot.line(ax=ax, hue='pool')
+    
+plt.show()
+# -
+
+ds_age_global_clean.mean_system_age.mean(dim=['lat', 'lon']).plot()
+
+ds_age_global_clean.mean_system_age.mean(dim=['ens', 'lat', 'lon']).plot()
+
+diff = (ds_age_global_clean.mean_age_vector/ds_age_global_clean.age_standard_deviation_vector)
+diff.mean(dim=['ens', 'lat', 'lon']).plot.line(hue='pool')
+
+diff = ds_age_global_clean.mean_system_age/ds_age_global_clean.system_age_standard_deviation
+diff.mean(dim=['ens', 'time']).plot()
+
+
