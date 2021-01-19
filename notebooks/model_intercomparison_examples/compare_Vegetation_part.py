e46c4baeea6c0a51dd1ec25d9c733d61c1a1dee1
Markus Müller <markus.mueller.1.g@googlemail.com>
2021-01-19 13:48:18 +0100

    started example notebook

---
+++ notebooks/model_intercomparison_examples/compare_Vegetation_part.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from IPython.display import HTML
+
+display(HTML("<style>.container { width:100% !important; }</style>"))
+
+# %load_ext autoreload
+# %autoreload 2
+import bgc_md2.helper as h
+
+model_inspection = h.MvarSetInspectionBox()
+
+model_list = h.MvarSetListGridBox(inspection_box=model_inspection)
+model_list
+
+
