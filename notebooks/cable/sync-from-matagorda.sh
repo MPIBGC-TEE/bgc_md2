2b05c4e8cdeb333bebe322d517c7fc04dcfc614b
Markus Müller <markus.mueller.1.g@googlemail.com>
2021-08-01 10:52:37 -0700

    just added the / so that rsync does not create a new directory

--- notebooks/cable/sync-from-matagorda.sh
+++ notebooks/cable/sync-from-matagorda.sh
@@ -1,3 +1,3 @@
-src_dir="/home/data/cable-data/example_runs/parallel_1901_2004_with_spinup/output/new4"
+src_dir="/home/data/cable-data/example_runs/parallel_1901_2004_with_spinup/output/new4/"
 target_dir=${src_dir}
 rsync -r --progress matagorda-from-home:${src_dir} ${target_dir} --exclude="zarr*"
