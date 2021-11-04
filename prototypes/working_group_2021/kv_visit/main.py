fixed errors in result representation; fixed ksi in the matrix formula

prototypes/working_group_2021/kv_visit/main.py
@@ -91,13 +91,13 @@ c_min = np.array(
         k_wood=1/(365*60),
         k_root=1/(365*30),
         k_leaf_lit=1/(365*60),
-        k_wood_lit=1/(365*10),
+        k_wood_lit=1/(365*30),
         k_root_lit=1/(365*30),
-        k_fast_som=1/(365*20),
-        k_slow_som=1/(365*50),
-        k_pass_som=1/(365*500),
+        k_fast_som=1/(365*200),
+        k_slow_som=1/(365*500),
+        k_pass_som=1/(365*1000),
         C_leaf_lit_0=0,
-        T_0=0.1,
+        T_0=-10,
         E=1,
         KM=1
     )
@@ -134,31 +134,34 @@ c_max = np.array(
 # I commented your original settings and instead used the 
 # values constructed from the limits (see below)
 epa_0 = EstimatedParameters(
-    beta_leaf=0.25,    #  1 (parameters used in original code)
-    beta_wood=0.2,    #  2
-    f_leaf_lit2fast_som=0.41,  #  3
-    f_leaf_lit2slow_som=0.07,#  4
-    f_leaf_lit2pass_som=0.02,#  5
-    f_wood_lit2fast_som=0.30,  #  6
-    f_wood_lit2slow_som=0.12,#  7
-    f_wood_lit2pass_som=0.08,#  8
-    f_root_lit2fast_som=0.30,  #  9
-    f_root_lit2slow_som=0.14,#  10
-    f_root_lit2pass_som=0.07,#  11
-    k_leaf=1/60,       #  12
-    k_wood=1/(365*12),       #  13
-    k_root=1/(365*5),       #  14
-    k_leaf_lit=1/(365*2),	#  15
-    k_wood_lit=1/(365*6.7),	#  16
-    k_root_lit=1/(365*3.5),	#  17
-    k_fast_som=1/(365*6.7),	#  18
-    k_slow_som=1/(365*28),	# 19
-    k_pass_som=1/(365*87),	# 20
-    C_leaf_lit_0=0.3,	# 21
-    T_0=2,	# 22
-    E=4,	# 23
-    KM=10  # 24
+    beta_leaf=0.6,    #  0 (parameters used in original code)
+    beta_wood=0.25,    #  1
+    f_leaf_lit2fast_som=0.41,  #  2
+    f_leaf_lit2slow_som=0.07,#  3
+    f_leaf_lit2pass_som=0.02,#  4
+    f_wood_lit2fast_som=0.30,  #  5
+    f_wood_lit2slow_som=0.12,#  6
+    f_wood_lit2pass_som=0.08,#  7
+    f_root_lit2fast_som=0.30,  #  8
+    f_root_lit2slow_som=0.14,#  9
+    f_root_lit2pass_som=0.07,#  10
+    k_leaf=1/(60*2),       #  11
+    k_wood=1/(365*30),       #  12
+    k_root=1/(365*22),       #  13
+    k_leaf_lit=1/(365*3.3),	#  14
+    k_wood_lit=1/(365*11),	#  15
+    k_root_lit=1/(365*11),	#  16
+    k_fast_som=1/(365*18),	#  17
+    k_slow_som=1/(365*100),	# 18
+    k_pass_som=1/(365*350),	# 19
+    C_leaf_lit_0=0.3,	# 20
+    T_0=2,	# 21
+    E=4,	# 22
+    KM=10  # 23
 )
+
+pd.DataFrame(epa_0).to_csv(dataPath.joinpath('epa_0.csv'),sep=',')
+
 # epa_0 = EstimatedParameters._make(
 #         np.concatenate(
 #             [
@@ -181,7 +184,7 @@ if not(isQualified(np.array(epa_0))):
 uniform_prop = make_uniform_proposer(
     c_min,
     c_max,
-    D=100,
+    D=240,
     filter_func=isQualified
 )
 
@@ -191,7 +194,7 @@ C_demo, J_demo = mcmc(
         param2res=param2res,
         #costfunction=make_weighted_cost_func(obs)
         costfunction=make_feng_cost_func(obs),
-        nsimu=4000
+        nsimu=5000
 )
 # save the parameters and costfunctionvalues for postprocessing 
 pd.DataFrame(C_demo).to_csv(dataPath.joinpath('visit_demo_da_aa.csv'),sep=',')
@@ -210,10 +213,8 @@ C_formal, J_formal = adaptive_mcmc(
         #nsimu=20000
         nsimu=10000
 )
-formal_aa_path = dataPath.joinpath('visit_formal_da_aa.csv')
-formal_aa_j_path = dataPath.joinpath('visit_formal_da_j_aa.csv')
-pd.DataFrame(C_formal).to_csv(formal_aa_path,sep=',')
-pd.DataFrame(J_formal).to_csv(formal_aa_j_path,sep=',')
+pd.DataFrame(C_formal).to_csv(dataPath.joinpath('visit_formal_da_aa.csv'),sep=',')
+pd.DataFrame(J_formal).to_csv(dataPath.joinpath('visit_formal_da_j_aa.csv'),sep=',')
 
 # POSTPROCESSING 
 #
@@ -262,13 +263,19 @@ fig.savefig('scatter_matrix.pdf')
 # A possible aggregation of this histogram to a singe parameter
 # vector is the mean which is an estimator of  the expected value of the
 # desired distribution.
+sol_init_par =param2res(epa_0)
 sol_mean =param2res(np.mean(C_formal,axis=1))
 sol_median =param2res(np.median(C_formal,axis=1))
 sol_min_J =param2res(C_formal[:,np.max(np.where(J_formal==np.min(J_formal)))])
 
+obs_path = dataPath.joinpath('obs.csv')
+sol_init_par_path = dataPath.joinpath('sol_init_par.csv')
 sol_mean_path = dataPath.joinpath('sol_mean.csv')
 sol_median_path = dataPath.joinpath('sol_median.csv')
 sol_min_J_path = dataPath.joinpath('sol_min_J.csv')
+
+pd.DataFrame(obs).to_csv(obs_path,sep=',')
+pd.DataFrame(sol_init_par).to_csv(sol_init_par_path,sep=',')
 pd.DataFrame(sol_mean).to_csv(sol_mean_path,sep=',')
 pd.DataFrame(sol_median).to_csv(sol_median_path,sep=',')
 pd.DataFrame(sol_min_J).to_csv(sol_min_J_path,sep=',')
@@ -283,290 +290,13 @@ plot_solutions(
 )
 fig.savefig('solutions.pdf')
 
-
-#!/usr/bin/env python
-import sys
-sys.path.insert(0,'..')
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from pathlib import Path
-import json
-
-
-from model_specific_helpers import (
-    get_example_site_vars,
-    make_param_filter_func,
-    #make_weighted_cost_func,
-    make_param2res,
-    #make_param2res_2,
-    UnEstimatedParameters,
-    EstimatedParameters,
-    Observables
-)
-
-from general_helpers import (
-        make_uniform_proposer,
-        make_multivariate_normal_proposer,
-        mcmc,
-        adaptive_mcmc,
-        make_feng_cost_func,
-        plot_solutions
-)
-
-with Path('config.json').open(mode='r') as f:
-    conf_dict=json.load(f)
-
-dataPath = Path(conf_dict['dataPath'])
-
-npp, C_leaf, C_wood, C_root, C_litter_above, C_litter_below, C_fast_som, C_slow_som, C_pass_som, \
-rh, f_veg2litter, f_litter2som, mrso, tsl = get_example_site_vars(dataPath)
-
-#nyears=150
-nyears = 10
-tot_len = 12*nyears
-obs_tup=Observables(
-    C_leaf=C_leaf,
-    C_wood=C_wood,
-    C_root=C_root,
-    C_litter_above=C_litter_above,
-    C_litter_below=C_litter_below,
-    C_fast_som=C_fast_som,
-    C_slow_som=C_slow_som,
-    C_pass_som=C_pass_som,
-    rh=rh,
-    f_veg2litter=f_veg2litter,
-    f_litter2som=f_litter2som
-)
-obs = np.stack(obs_tup, axis=1)[0:tot_len,:]
-
-cpa = UnEstimatedParameters(
-    C_leaf_0=C_leaf[0],
-    C_wood_0=C_wood[0],
-    C_root_0=C_root[0],
-    C_litter_above_0=C_litter_above[0],
-    C_litter_below_0=C_litter_below[0],
-    C_fast_som_0=C_fast_som[0],
-    C_slow_som_0=C_slow_som[0],
-    C_pass_som_0=C_pass_som[0],
-    rh_0=rh[0],
-    f_veg_lit_0=f_veg2litter[0],
-    f_lit_soil_0=f_litter2som[0],
-    npp=npp,
-    number_of_months=tot_len,
-    mrso=mrso,
-    tsl=tsl
-)
-param2res = make_param2res(cpa)
-
-c_min = np.array(
-    EstimatedParameters(
-        beta_leaf=0,
-        beta_wood=0,
-        f_leaf_lit2fast_som=0.1,
-        f_leaf_lit2slow_som=0.01,
-        f_leaf_lit2pass_som=0.001,
-        f_wood_lit2fast_som=0.1,
-        f_wood_lit2slow_som=0.01,
-        f_wood_lit2pass_som=0.001,
-        f_root_lit2fast_som=0.1,
-        f_root_lit2slow_som=0.01,
-        f_root_lit2pass_som=0.001,
-        k_leaf=1/(365*2),
-        k_wood=1/(365*60),
-        k_root=1/(365*30),
-        k_leaf_lit=1/(365*60),
-        k_wood_lit=1/(365*10),
-        k_root_lit=1/(365*30),
-        k_fast_som=1/(365*20),
-        k_slow_som=1/(365*50),
-        k_pass_som=1/(365*500),
-        C_leaf_lit_0=0,
-        T_0=0.1,
-        E=1,
-        KM=1
-    )
-)
-
-c_max = np.array(
-    EstimatedParameters(
-        beta_leaf=1,
-        beta_wood=1,
-        f_leaf_lit2fast_som=0.9,
-        f_leaf_lit2slow_som=0.9,
-        f_leaf_lit2pass_som=0.9,
-        f_wood_lit2fast_som=0.9,
-        f_wood_lit2slow_som=0.2,
-        f_wood_lit2pass_som=0.2,
-        f_root_lit2fast_som=0.9,
-        f_root_lit2slow_som=0.9,
-        f_root_lit2pass_som=0.9,
-        k_leaf=1/30,
-        k_wood=1/(365*1),
-        k_root=1/(365*0.5),
-        k_leaf_lit=1/(365*1),
-        k_wood_lit=1/(365*0.5),
-        k_root_lit=1/(365*0.5),
-        k_fast_som=1/(365*1),
-        k_slow_som=1/(365*3.5),
-        k_pass_som=1/(365*10),
-        C_leaf_lit_0=0.4,
-        T_0=4,
-        E=100,
-        KM=100
-    )
-)
-# I commented your original settings and instead used the
-# values constructed from the limits (see below)
-epa_0 = EstimatedParameters(
-    beta_leaf=0.25,    #  1 (parameters used in original code)
-    beta_wood=0.2,    #  2
-    f_leaf_lit2fast_som=0.41,  #  3
-    f_leaf_lit2slow_som=0.07,#  4
-    f_leaf_lit2pass_som=0.02,#  5
-    f_wood_lit2fast_som=0.30,  #  6
-    f_wood_lit2slow_som=0.12,#  7
-    f_wood_lit2pass_som=0.08,#  8
-    f_root_lit2fast_som=0.30,  #  9
-    f_root_lit2slow_som=0.14,#  10
-    f_root_lit2pass_som=0.07,#  11
-    k_leaf=1/60,       #  12
-    k_wood=1/(365*12),       #  13
-    k_root=1/(365*5),       #  14
-    k_leaf_lit=1/(365*2),	#  15
-    k_wood_lit=1/(365*6.7),	#  16
-    k_root_lit=1/(365*3.5),	#  17
-    k_fast_som=1/(365*6.7),	#  18
-    k_slow_som=1/(365*28),	# 19
-    k_pass_som=1/(365*87),	# 20
-    C_leaf_lit_0=0.3,	# 21
-    T_0=2,	# 22
-    E=4,	# 23
-    KM=10  # 24
-)
-# epa_0 = EstimatedParameters._make(
-#         np.concatenate(
-#             [
-#                 # we don't want to use the average for
-#                 # the betas and fs since their sum will immediately
-#                 # violate our filter condition 3,4,5,6
-#                 np.array((0.25, 0.2, 0.42,0.075, 0.005, 0.35, 0.12,  0.03, 0.37, 0.11, 0.01)),
-#                 # but for the rest it se
-#                 (c_min+c_max)[11:] / 2.0
-#             ]
-#         )
-# )
-
-isQualified = make_param_filter_func(c_max,c_min)
-# check if the current value passes the filter
-# to avoid to get stuck in an inescapable series of rejections
-if not(isQualified(np.array(epa_0))):
-    raise ValueError("""the current value does not pass filter_func. This is probably due to an initial value chosen outside the permitted range""")
-
-uniform_prop = make_uniform_proposer(
-    c_min,
-    c_max,
-    D=30,
-    filter_func=isQualified
-)
-
-C_demo, J_demo = mcmc(
-        initial_parameters=epa_0,
-        proposer=uniform_prop,
-        param2res=param2res,
-        #costfunction=make_weighted_cost_func(obs)
-        costfunction=make_feng_cost_func(obs),
-        nsimu=4000
-)
-# save the parameters and costfunctionvalues for postprocessing
-pd.DataFrame(C_demo).to_csv(dataPath.joinpath('visit_demo_da_aa.csv'),sep=',')
-pd.DataFrame(J_demo).to_csv(dataPath.joinpath('visit_demo_da_j_aa.csv'),sep=',')
-
-# build a new proposer based on a multivariate_normal distribution using the estimated covariance of the previous run if available
-# parameter values of the previous run
-
-C_formal, J_formal = adaptive_mcmc(
-        initial_parameters=epa_0,
-        covv=np.cov(C_demo[:, int(C_demo.shape[1]/10):]),
-        filter_func = isQualified,
-        param2res=param2res,
-        #costfunction=make_weighted_cost_func(obs)
-        costfunction=make_feng_cost_func(obs),
-        #nsimu=20000
-        nsimu=10000
-)
-formal_aa_path = dataPath.joinpath('visit_formal_da_aa.csv')
-formal_aa_j_path = dataPath.joinpath('visit_formal_da_j_aa.csv')
-pd.DataFrame(C_formal).to_csv(formal_aa_path,sep=',')
-pd.DataFrame(J_formal).to_csv(formal_aa_j_path,sep=',')
-
-# POSTPROCESSING
-#
-# The 'solution' of the inverse problem is actually the (joint) posterior
-# probability distribution of the parameters, which we approximate by the
-# histogram consisting of the mcmc generated samples.
-# This joint distribution contains as much information as all its (infinitly
-# many) projections to curves through the parameter space combined.
-# Unfortunately, for this very reason, a joint distribution of more than two
-# parameters is very difficult to visualize in its entirity.
-# to do:
-#   a) make a movie of color coded samples  of the a priori distribution of the parameters.
-#   b) -"-                                  of the a posteriory distribution -'-
-
-# Therefore the  following visualizations have to be considered with caution:
-# 1.
-# The (usual) histograms of the values of a SINGLE parameters can be very
-# misleading since e.g. we can not see that certain parameter combination only
-# occure together. In fact this decomposition is only appropriate for
-# INDEPENDENT distributions of parameters in which case the joint distribution
-# would be the product of the distributions of the single parameters.  This is
-# however not even to be expected if our prior probability distribution can be
-# decomposed in this way. (Due to the fact that the Metropolis Hastings Alg. does not
-# produce independent samples )
-df = pd.DataFrame({name :C_formal[:,i] for i,name in enumerate(EstimatedParameters._fields)})
-subplots=df.hist()
-fig=subplots[0,0].figure
-fig.set_figwidth(15)
-fig.set_figheight(15)
-fig.savefig('histograms.pdf')
-
-# As the next best thing we can create a matrix of plots containing all
-# projections to possible  parameter tuples
-# (like the pairs plot in the R package FME) but 16x16 plots are too much for one page..
-# However the plot shows that we are dealing with a lot of colinearity for this  parameter set
-subplots = pd.plotting.scatter_matrix(df)
-fig=subplots[0,0].figure
-fig.set_figwidth(15)
-fig.set_figheight(15)
-fig.savefig('scatter_matrix.pdf')
-
-
-# 2.
-# another way to get an idea of the quality of the parameter estimation is
-# to plot trajectories.
-# A possible aggregation of this histogram to a singe parameter
-# vector is the mean which is an estimator of  the expected value of the
-# desired distribution.
-sol_mean =param2res(np.mean(C_formal,axis=1))
-sol_median =param2res(np.median(C_formal,axis=1))
-sol_min_J =param2res(C_formal[:,np.max(np.where(J_formal==np.min(J_formal)))])
-
-sol_mean_path = dataPath.joinpath('sol_mean.csv')
-sol_median_path = dataPath.joinpath('sol_median.csv')
-sol_min_J_path = dataPath.joinpath('sol_min_J.csv')
-pd.DataFrame(sol_mean).to_csv(sol_mean_path,sep=',')
-pd.DataFrame(sol_median).to_csv(sol_median_path,sep=',')
-pd.DataFrame(sol_min_J).to_csv(sol_min_J_path,sep=',')
-
+############################# plot solution with initial parameters ######################
 fig = plt.figure()
 plot_solutions(
         fig,
-        times=range(sol_mean.shape[0]),
+        times=range(sol_init_par.shape[0]),
         var_names=Observables._fields,
-        tup=(sol_mean, obs),
+        tup=(sol_init_par, obs),
         names=('mean','obs')
 )
-fig.savefig('solutions.pdf')
-
-
+fig.savefig('solutions_init.pdf')