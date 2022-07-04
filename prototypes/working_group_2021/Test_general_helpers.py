fixed the problems with the jules coordinate system and the slow computation of the common mask. NAN pixels are now checked for

prototypes/working_group_2021/Test_general_helpers.py
@@ -281,6 +281,7 @@ class Test_general_helpers(InDirTest):
         ref_mask[0,0]=True
         ref_mask[2,3]=True
         ref_mask[1,3]=True
+        
         res = gh.get_nan_pixel_mask(test_var)
         self.assertTrue(
             (ref_mask == res).all()
@@ -320,6 +321,12 @@ class Test_general_helpers(InDirTest):
         lon = ds.createDimension('lon',size=n_lons)
         test_var=ds.createVariable("test_var",np.float64,['time','depth','lat','lon'])
         test_var[:,:,:,:]=ma_arr
+        
+        res = gh.get_nan_pixel_mask(test_var)
+        from IPython import embed;embed()
+        self.assertTrue(
+            (ref_mask == res).all()
+        )
         ds.close()
 
 
@@ -988,14 +995,14 @@ class Test_general_helpers(InDirTest):
         
     def test_read_or_create(self):
         cachePath=Path("cache.nc")
- 
+        var_name="test" 
         def caw(path):
             sleep(2)
             n = 5
             ds = nc.Dataset(path,'w',persist=True)
             lat = ds.createDimension('lat',size=n)
             lon = ds.createDimension('lon',size=n)
-            test=ds.createVariable("test",np.float64,['lat','lon'])
+            test=ds.createVariable(var_name,np.float64,['lat','lon'])
             var=np.diag(np.arange(0,n))
             test[:,:]=var
             return var 
@@ -1003,9 +1010,9 @@ class Test_general_helpers(InDirTest):
         
         def r(path):
             ds=nc.Dataset(path)
-            return ds.variables['test'][:,:].data
+            return ds.variables[var_name][:,:].data
         
-        path=Path("test.nc")
+        path=Path("cache","test.nc")
         before=time()
         res_1=gh.read_or_create(
                 path=path,

prototypes/working_group_2021/general_helpers.py
@@ -1126,6 +1126,100 @@ def plot_observations_vs_simulations(
         axs[i].set_title(name)
 
 
+def reduce_is_infinite_over_first_dim(
+        arr: np.ma.core.MaskedArray
+    ) -> np.ma.core.MaskedArray :
+    # 
+    return  reduce(
+        lambda acc,el: np.logical_or(
+            acc,
+            np.logical_not(
+                np.isfinite(el)
+            )
+        )
+        ,
+        map(lambda i:arr[i], range(arr.shape[0]))
+    )
+
+
+def is_infinite_rec(
+        chunk: np.ma.core.MaskedArray
+    ) -> np.ma.core.MaskedArray:
+    # works for any dimension (some variables are 4, most are 3 dimensional)
+    # The function reduce_is_infinite_over_first_dim reduces the array dimension by 1 
+    # everytime it is called and is called
+    # until the result is a 2d array (lat,lon are always the last dimensions in our datasets)
+    if chunk.ndim==2:
+        return chunk
+    else:
+        return is_infinite_rec(
+            reduce_is_infinite_over_first_dim(chunk)
+        )
+
+
+def get_nan_pixel_mask(
+        var: Union[nc._netCDF4.Variable,np.ma.core.MaskedArray]
+    )->np.ndarray:
+    try: 
+        # only works if var is a netCDF4.Variable
+        print(var.name) 
+    except:
+        AttributeError()
+
+
+    # partition along the the chunks of the netcdf file (measured about a100 times faster)
+    # and much less memory consuming than reading the complete array which requiers more than 16GB RAM
+    
+    try:
+        chunksizes=var.chunking()
+        chunksize = 1 if chunksizes == 'contiguous' else chunksizes[0]
+    except:
+        # chunksizes could still be None
+        TypeError
+        chunksize=1
+    # Note: no list comprehension but a lazy map object to save memory
+    subarrs=map(
+        lambda t: var[t[0]:t[1]],
+        partitions(
+            start=0,
+            stop=var.shape[0],
+            nr_acc=chunksize
+        )
+    )
+    
+    res=reduce(
+        lambda acc, el: np.logical_or(acc,el),
+        tqdm(map(is_infinite_rec,subarrs))
+    )
+    # res is a masked array where both res.data and res.mask  
+    # are boolean arrays. res.mask is the logical_or for any masked
+    # array operation and in our case logical_or is also applied to the 
+    # data (in the reduce call in this function and is_finite_rec)
+    
+    # the ultimate result is a mask that masks every lat,lon pixel 
+    # that is already masked for any time or any layer
+    # or has a nan entry at any time or any layer
+    mask=np.logical_or(res.data,res.mask)
+
+    # we check if the original mask was appropriate
+    org_mask_ind = tuple(0 for i in range(var.ndim-2))+(slice(None),)+(slice(None),)
+    org_mask=var[org_mask_ind].mask
+
+    unmasked_nan_pixels=np.argwhere(
+        np.logical_and(
+            mask,
+            np.logical_not(org_mask)
+        )
+    )
+    if len(unmasked_nan_pixels)>0:
+        print(
+            "found {} previously unmasked NaN pixels {}".format(
+                len(unmasked_nan_pixels),
+                unmasked_nan_pixels
+            )
+        )
+    return mask
+
 def get_nan_pixels(
         var: nc._netCDF4.Variable
     ):
@@ -1154,75 +1248,25 @@ def get_nan_pixels(
     )
     return reduce(lambda x,y:x+y,l)
 
-def reduce_dims(
-        var: Union[nc._netCDF4.Variable, np.ma.core.MaskedArray] ,
-        min_dim: int,
-        f_data: Callable,#[[Array,Array],Array],
-        f_mask: Callable,#[[Array,Array],Array]
-    ):
-    # small recursive helper that applies the function f in a reduce call
-    # reduce(f(acc,el),[arr[0,:,:...] ,arr[1,:,:,...]]) 
-    # where the elements of the list are the subarrays arr[0],arr[1]...
-    # where the accumulater acc has one dimensions less  
-    # (for example summing: f(acc,el) = acc+ell 
-    # applying f over the first dimension reduces the dimension of the original array by one
-    #
-    # The function calls itself again until the number of dimensions has reached min_dim 
-    n=var.ndim    
-    if n == min_dim:
-        return var
-    else:
-        s0=var.shape[0]
-        start_var=var[0] # note that in numpy this is equivalent to var[0,:,:...]
-        def g(acc,i):
-            el=var[i]
-            return np.ma.masked_array(
-                data=f_data(acc.data,el.data),
-                mask=f_mask(acc.mask,el.mask)
-            )
-        new_ma = reduce(
-            g,
-            tqdm(range(s0)),
-            start_var
+def reduce_is_infinite_over_first_dim(arr):
+    def f(acc, el):
+        new_acc=np.logical_or( acc,el)
+        new_ind=np.argwhere(np.logical_not(new_acc==acc)) 
+        #if len(new_ind)>0:
+        #    print(new_ind)
+        return new_acc
+            
+    def make_sub_arr_mask(i):
+        #print(i)
+        return np.logical_not(
+            np.isfinite(arr[i])
         )
 
-        return reduce_dims(
-            var=new_ma,
-            min_dim=min_dim,
-            f_data=f_data,
-            f_mask=f_mask
-        )
+    return  reduce(
+        f,
+        map(make_sub_arr_mask, range(arr.shape[0]))
+    )
 
-def get_nan_pixel_mask(
-        var: nc._netCDF4.Variable
-    ):
-    ## We use a netCDF4.Variable to avoid having to load the whole array into memory 
-    ## since it is too big e.g. for the dlm files 
-
-    
-    def f_data(acc,el):
-        return np.logical_or(
-            acc,
-            np.logical_not(
-                np.isfinite(el)
-            )
-        )
-
-    
-    def f_mask(acc,el):
-        return np.logical_or(
-            acc,
-            el
-        )
-
-    m_arr= reduce_dims(
-            var,
-            min_dim=2,
-            f_data=f_data,
-            f_mask=f_mask
-    ) # the result is a masked array 
-
-    return np.logical_or(m_arr.mask,m_arr.data)
 
 
 
@@ -2237,6 +2281,29 @@ def transform_maker(lat_0,lon_0,step_lat,step_lon):
         #lon2i=lon2i,
     )
 
+def read_or_create(
+            path: Path,
+            create_and_write: Callable[[Path],T],
+            read: Callable[[Path],T]
+    ) -> T:
+    """
+    A the basic cache functionality:
+    path: the path of the cache file
+    create_and_write: The funciton that creates the  object to be cached and writes it to <path>.
+    read: The function that extracts the cached object from <path>.
+    """
+    print('type',type(path))
+    if path.exists():
+        print(
+                "Found cache file {}. If you want to recompute the result remove it.".format(str(path)) 
+        )
+        return read(path)
+    else:
+        pp=path.parent
+        if not(pp.exists()): 
+            pp.mkdir(parents=True)
+        return create_and_write(path)
+
 # +
 # For harmonizing timelines and plotting
 # -
@@ -2290,7 +2357,6 @@ def t_min_tmax_full(test_arg_list, # a list of test_args from all models involve
     return (t_min,t_max)
 
 # function find the timesteps corresponding to shared times
-from functools import reduce
 def min_max_index(test_args, # a tuple defined in model-specific test_helpers.py
                   delta_t_val, # iterator time step
                   t_min, # output of t_min_tmax_overlap or t_min_tmax_full
@@ -2846,18 +2912,4 @@ def plot_diff(model_names, # dictionary (folder name : model name)
                 ax.plot(times,diff,color="black")
                 ax.set_title("Delta {0} for {1}-{2}".format(name,model_names[mf_1],model_names[mf_2]))
                 plot_number+=1 
-
-def read_or_create(
-            path: Path,
-            create_and_write: Callable[[Path],T],
-            read: Callable[[Path],T]
-    ) -> T:
-    print('type',type(path))
-    if path.exists():
-        print(
-                "Found cache file {}. If you want to recompute the result remove it.".format(str(path)) 
-        )
-        return read(path)
-    else:
-        return create_and_write(path)
 

prototypes/working_group_2021/yz_jules/model_specific_helpers_2.py
@@ -48,12 +48,10 @@ def spatial_mask(dataPath)->'CoorMask':
     # we now check if any of the arrays has a time lime containing nan values 
     # APART FROM values that are already masked by the fillvalue
     
-    # 1.)
-    f_mask=nc.Dataset(dataPath.joinpath("JULES-ES-1p0_S2_cSoil.nc")).variables['cSoil'][0,:,:].mask
-    
-    # 2.) 
     print("computing masks to exclude pixels with nan entries, this may take some minutes...")
     
+
+    # We compute the common mask so that it yields valid pixels for ALL variables 
     def f(d_name):
         vn_in_file = d_name2varname_in_file[d_name]
         file_name = file_name_from_var_name[vn_in_file]
@@ -68,11 +66,8 @@ def spatial_mask(dataPath)->'CoorMask':
     names = o_names + d_names 
     
     masks=[ f(name)    for name in names ]
-    from IPython import embed;embed() 
+    combined_mask = reduce(lambda acc,m: np.logical_or(acc,m),masks)
 
-    # We compute the common mask so that it yields valid pixels for ALL variables 
-    combined_mask= reduce(lambda acc,m: np.logical_or(acc,m),masks,f_mask)
-    print("found additional {} NaN pixels".format(combined_mask.sum()-f_mask.sum()))
     sym_tr= gh.SymTransformers(
         itr=make_model_index_transforms(),
         ctr=make_model_coord_transforms()
@@ -81,6 +76,7 @@ def spatial_mask(dataPath)->'CoorMask':
         combined_mask,
         sym_tr
     )
+
 def make_model_coord_transforms():
     """ This function can is used to achieve a target grid LAT,LON with
     - LAT ==   0 at the equator with 
@@ -92,8 +88,8 @@ def make_model_coord_transforms():
     return gh.CoordTransformers(
             lat2LAT=lambda lat: lat,
             LAT2lat=lambda LAT: LAT,
-            lon2LON=lambda lon: lon-180,
-            LON2lon=lambda LON: LON+180,
+            lon2LON=lambda lon: -180+ lon-180 if lon > 180 else lon,
+            LON2lon=lambda LON: 360+LON if LON < 0 else LON
     )
     
 def make_model_index_transforms():