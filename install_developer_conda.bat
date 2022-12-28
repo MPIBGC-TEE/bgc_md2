7ba60a6d6630a13891e09b570b97ff7e5b438314
Markus Mueller <markus.mueller.1.g@googlemail.com>
2022-12-27 20:58:34 -0500

    new python version

--- install_developer_conda.bat
+++ install_developer_conda.bat
@@ -1,6 +1,6 @@
 @echo off
-echo installing python 3.8
-call conda install -y -c conda-forge python=3.8
+echo installing python 3.10.9
+call conda install -y -c conda-forge python=3.10.9
 set back=%cd%
 for /d %%i in ("src\testinfrastructure", "src\ComputabilityGraphs", "src\LAPM", "src\CompartmentalSystems") do (
 	cd "%%i"
