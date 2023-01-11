c2de30cfc33934be2638bca9cb98ddd257d7e951
Markus Müller <markus.mueller.1.g@googlemail.com>
2023-01-11 16:49:24 -0500

    now it's easy to update the pinned environment

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -1,2 +1,2 @@
 conda env remove -n binder
-conda env create -f environment.yml && conda activate binder && ./postBuild && conda env export > environment.yml
+conda env create -f environment_free.yml && conda activate binder && ./postBuild && conda env export > environment.yml
