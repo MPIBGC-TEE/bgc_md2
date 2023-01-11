5e618ca39107b611982b431d84b59a6e9baae709
Markus Müller <markus.mueller.1.g@googlemail.com>
2023-01-11 15:56:25 -0500

    test

--- .github/workflows/run_test_suites.sh
+++ .github/workflows/run_test_suites.sh
@@ -1,16 +1,17 @@
+fail=0
 in_dir_command(){
 	cd $1
 	echo "################################################################################"
 	echo $(pwd)
 	echo $2
 	eval $2
+	fail = $?
 	cd -
 }
   
 in_dir_command tests "python -m unittest discover -t . -p 'Test*'"
-    
 in_dir_command prototypes/working_group_2021 "python -m unittest Test_general_helpers.py"
-
 in_dir_command notebooks "pytest --nbmake './'"
 
-        
+# make sure that the script has non-zero exit status if one of the suites has failing test
+test(fail)        
