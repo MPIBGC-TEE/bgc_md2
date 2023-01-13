27acc1f3d0033723e8ca6e2d61e8ce1ee9c6139b
Markus Müller <markus.mueller.1.g@googlemail.com>
2023-01-12 19:41:31 -0500

    test

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -1,6 +1,3 @@
-# this file wants to be sourced! (otherwise the conda activate does not work)
-conda config --add channels anaconda
-conda config --add channels conda-forge
 conda update -y -n base -c defaults conda
 conda activate base
 conda env remove -n binder
