2a37aa72188b36fcd225f29d770698f22f4c3b7c
Markus Müller <markus.mueller.1.g@googlemail.com>
2023-01-12 19:12:12 -0500

    test

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -1,5 +1,7 @@
+# this file wants to be sourced! (otherwise the conda activate does not work)
 conda config --add channels anaconda
 conda config --add channels conda-forge
 conda update -y -n base -c defaults conda
+conda activate base
 conda env remove -n binder
 conda env create -f environment_free.yml && conda activate binder && ./postBuild && conda env export > environment.yml
