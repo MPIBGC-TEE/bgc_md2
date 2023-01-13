7fe8c178eecd009c8694ad82128cb5ce7501c077
Markus Müller <markus.mueller.1.g@googlemail.com>
2023-01-12 21:10:04 -0500

    test

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -1,3 +1,3 @@
-conda update -y -n base -c defaults conda
+mamba update
 mamba env remove -n binder
 mamba env create -f environment_free.yml && conda activate binder && ./postBuild && mamba env export > environment.yml
