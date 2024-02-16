ecc2176789b7beb88fc878d1868eec3976e9b7ff
Markus Müller <markus.mueller.1.g@googlemail.com>
2024-02-16 11:20:39 +0100

    updated the minimodel

--- prototypes/mini_model.py
+++ prototypes/mini_model.py
@@ -6,13 +6,14 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.5
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+from IPython.display import display, Markdown, Math
 from sympy import var,Function,Symbol,Rational
 from ComputabilityGraphs.CMTVS import CMTVS
 from bgc_md2.helper import bgc_md2_computers
@@ -38,22 +39,26 @@ params_in.close()
 #define all the symbols you are going to use in the equations
 
 for k in sym_dict.keys():
-    code=k+" = Symbol('{0}')".format(k)
+    code = k+" = Symbol('{0}')".format(k)
     exec(code)
     
 func_dict = {
-    "Gpp_grass": "Total C production for grass in g/m^2*month FIXME"
+    "gpdf": "function for calculating temperature effect on production",
+    "scale": "function called in treemix.F and cropmix.F"
 }
 for k in func_dict.keys():
-    code=k+" = Function('{0}')".format(k)
-    exec(code)    
+    code = k+" = Function('{0}')".format(k)
+    exec(code)  
     
-#I_wood=Function("I_wood") 
-t=TimeSymbol("t") # the symbol used for time since it has a special role
+gpdf = Function("gpdf")
+scale = Function("scale") 
+t = TimeSymbol("t") # the symbol used for time since it has a special role
 e = Symbol("e")   # for exponential functions
 
-agprod = prdx_1*f_stemp_ppdf_1*((avh2o_1 + (ppt_m * 0.1)) / pet)*biof*shdmod*sdlng
-bgprod = agprod * rtsh
+potprd = c3c4*gpdf(stemp,ppdf1_C3,ppdf2_C3,ppdf3_C3,ppdf4_C3) + (1-c3c4)*gpdf(stemp,ppdf1_C4,ppdf2_C4,ppdf3_C4,ppdf4_C4)
+prdx_1 = scale(c3c4, prdx_C3, prdx_C4)
+agprod = prdx_1*potprd*((avh2o_1 + (ppt_m * 0.1)) / pet)*biof*shdmod*sdlng
+bgprod = agprod*rtsh
 cprodc = (agprod + bgprod) * 0.4
 cprodf = ((prdx_2*f_stemp_ppdf_2*((avh2o_2 + (ppt_m * 0.1)) / pet)*f_lait_laitop*0.5) - sumrsp)
 
@@ -187,19 +192,187 @@ compartmental_graph(mvs)
 from bgc_md2.display_helpers import mass_balance_equation
 mass_balance_equation(mvs)
 
+# +
 # for comparison the century model as found in our database
-from bgc_md2.models.Parton1987SoilSciSocAmJ.source_by_name import mvs as mvs_century
+#from bgc_md2.models.Parton1987SoilSciSocAmJ.source_by_name import mvs as mvs_century
+#mvs.computable_mvar_types
+#mvs_century.get_InputTuple()
+#compartmental_graph(mvs_century)
+#mass_balance_equation(mvs_century)
+#BI=mvs_century.get_BibInfo()
+#BI.sym_dict
 
-mvs.computable_mvar_types
+# +
+# I will just create a dictionary with completely arbitrary values for the parameters
+# and one for the functions 
+# Don't get a heart attack .. how ignorant I might be about the TRUE values... 
+# This is just a guide for the technical side
 
-mvs_century.get_InputTuple()
+# first I find out which parameters there are...
+# Any sympy expression has a free_symbols method and I implemented a similar one
+# for the symbolic model class. Its a shortcut that looks at matrix and Inputs 
+# and removes the statevariables automatically
+srm=mvs.get_SmoothReservoirModel()
+srm.free_symbols
+# Of course you would probably know this but this little test is useful half way through 
+# to check which parameters are still in need of a value...
 
-compartmental_graph(mvs_century)
+# +
+# In real live you would probably read this from a textfile
+# ... yes, read parameter dictionary from a txt file
+import ast
+par_dict_in = open(r"mini_model_params_dict.txt", 'r')
+par_dict = {}
+for line in par_dict_in:
+    k, v = line.strip().split('=')
+    par_dict[k.strip()] = ast.literal_eval(v)
+    
+par_dict_in.close()
+# -
 
-mass_balance_equation(mvs_century)
+par_dict
 
-BI=mvs_century.get_BibInfo()
-BI.sym_dict
+# The functions are a bit more obvious since we only expressed a view of them explicitly 
+# but we could also find them as the little example schows...
+test_f = Function("test_f")
+x = Symbol("x")
+expr = x**2*test_f(x)
+expr, expr.atoms(Function)
+
+# Now for our real model, where functions could be in the  
+mvs.get_InputTuple().atoms(Function).union(mvs.get_CompartmentalMatrix().atoms(Function))
+
+import numpy as np
+import matplotlib.pyplot as plt
+# It seems that the G_pp_grass function is no longer here 
+# but I demonstrate how to build a concrete numeric version of this symbolic function,
+# which will in real live in all likelyhood be an interpolation from data (probably from a netcdf file)
+# We can build such a function later when you tell me about your data format.
+# Here I invented  a seasonal function with a peak in the middle of the year
+def num_G_pp_grass(t):
+    omega=2*np.pi/365 #period of 365 days  
+    phi=np.pi # phaseshift to start at the lowest value at t=0
+    return 1+np.cos(omega*t + phi)
+ts=np.array(range(730))
+ax=plt.plot(ts,num_G_pp_grass(ts))
+#func_dict={Gpp_grass(t): num_G_pp_grass}
+# but since we have not functions left we define an empty func_dict
+#func_dict=dict()
+
+# +
+# functions from century
+import numpy as np
+
+# I was a bit suspicious of the functions, in particular that they are proper nuerical functions, that we could plot.
+# This turned out not to be the problem which was due to naming...
+# In fact the symbolic version and the numeric one are different objects but referred to by the same name 
+# (which is a problem only if the two objects are in the same namespace which in this notebook they were)
+# So the real problem was solved by renaming the numeric version. (I choose the arbritrary suffix "_num")
+
+
+# But plotting is a good test anyway... since pythons leniency to accept everything is a bit of a problem when it fails much later...
+# Of course plotting a function over a five dimensionsal domain would earn you the Fields Medal but we can
+# at least try to plot the restrictions of the function to one of the variables.
+def gpdf_num(x,a,b,c,d):
+    frac = (b-x)/(b-a)
+    if frac>0:
+        return (pow(frac,c))*np.exp((c/d)*(1-pow(frac,d)))
+    else:
+        return 0
+
+fig=plt.figure()
+axs=fig.subplots(5,1)
+xs=np.linspace(-10,10,210) # made up values, you should put something realistic here
+ys=[gpdf_num(x=x,a=2,b=3,c=4,d=5) for x in xs] # dito....
+axs[0].plot(xs,ys)
+
+a_s=np.linspace(-5,5,210) #dito
+ys=[gpdf_num(x=2,a=a,b=3,c=4,d=5) for a in a_s]
+axs[1].plot(a_s,ys)
+
+
+# etc.....(plot other restrictions, maybe also a 3d plot of two parameters...)
+# -
+
+def scale_num(fac, val1, val2):
+    return (fac*val1) + ((1-fac) * val2)
+fig2=plt.figure()
+axs=fig2.subplots(3,1)
+facs=np.linspace(-10,10,210)
+ys=[scale_num(fac,1,2) for fac in facs]
+axs[0].plot(facs,ys)
+# etc..
+
+#actually sympy figures out the arguments by position (I could have ommitted them in my example) 
+func_dict = {
+    gpdf: gpdf_num,
+    scale: scale_num
+}
+
+# +
+#func_dict = {
+#    gpdf(stemp,ppdf1_C3,ppdf2_C3,ppdf3_C3,ppdf4_C3): gpdf,
+#    gpdf(stemp,ppdf1_C4,ppdf2_C4,ppdf3_C4,ppdf4_C4): gpdf, 
+#    scale(c3c4, prdx_C3, prdx_C4): scale
+#}
+# -
+
+
+
+# The last thing we need to run the model are the startvalues
+# These are found in the .100 files in the ModelParameters_MC2 directory by veg type; see sumcar.F for equivalent parameter names.
+# METABC and STRUCC are initialized in calciv.F.
+svd={
+    AGLIVC:0, 
+    BGLIVC:200, 
+    STDEDC:50, 
+    STRUCC_1:0, 
+    STRUCC_2:0, 
+    SOM1C_1:25,
+    SOM1C_2:35,
+    SOM2C:2400,
+    SOM3C:1400,
+    METABC_1:0,
+    METABC_2:0,
+    CROOTC:500,
+    FBRCHC:150,
+    FROOTC:524,
+    RLEAVC:28,
+    RLWODC:300,
+    WOOD1C:180,
+    WOOD2C:100,
+    WOOD3C:100
+}
+svt=mvs.get_StateVariableTuple()
+#svt
+
+# We make a small test that we have all startvalues of them by substituting the values  for the symbols
+svt.subs(svd)
+
+# +
+# Now we can update our mvs object (CMTVS Connected Multi Type Variable Set) 
+from bgc_md2.resolve.mvars import NumericParameterization,NumericStartValueDict,NumericSimulationTimes
+mvs=mvs.update(
+    [
+        NumericParameterization(par_dict=par_dict,func_dict=func_dict),
+        NumericStartValueDict(svd),
+        NumericSimulationTimes(ts)
+    ]
+)
+
+sa=mvs.get_NumericSolutionArray()
+_, n_pools=sa.shape
+fig = plt.figure(figsize=(10,10*n_pools))
+axs=fig.subplots(n_pools)
+for i in range(n_pools):
+    ax=axs[i]
+    ax.plot(ts,sa[:,i])
+    ax.set_title(svt[i])
+# -
+
+
+
+
 
 x=Symbol("x")
 
