a6e3499b4b9074f5fd1b46d1687a87fc99f4ef6c
Markus Müller <markus.mueller.1.g@googlemail.com>
2025-07-08 12:53:30 +0200

    new workflow files

--- tests/run_in_parallel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# needs unittest-parallel installed
-# -j 0 is the default and meanss all cores
-# --level 'test' means parallelize on test level
-unittest-parallel  --level 'test' --pattern 'Test*'
-
